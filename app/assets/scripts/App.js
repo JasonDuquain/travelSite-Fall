c1ed502aa99ce72e871f963c7c7916774fcea957
jasonduquain <jduquain@gmail.com>
2017-09-28 11:24:16 -0400

    sticky hdr and highlite nav on scroll done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,5 @@
 /*MobileMenu.js*/
 
-
-
 class MobileMenu {
     constructor() {
         this.siteHeader = document.querySelector('.site-header');
@@ -24,3 +22,89 @@ class MobileMenu {
 }
 
 var mobileMenu = new MobileMenu();
+
+
+/* RevealOnScroll.js*/
+
+class RevealOnScroll {
+    constructor() {
+        this.itemsToReveal = document.querySelectorAll('.feature-item, .testimonial');
+        this.hideInitially();
+        this.showElements();
+    }
+    
+    hideInitially() {
+        this.itemsToReveal.forEach( (item) => {
+           item.classList.add('reveal-item'); 
+        });
+    }
+    
+    showElements() {
+       this.itemsToReveal.forEach( (item) => {
+           document.addEventListener('scroll', (e) => {
+               if (item.classList.contains('testimonial')) {
+                   if (item.getBoundingClientRect().top <= 300) {
+                    item.classList.add('reveal-item--is-visible');
+                    }
+                } else if (item.getBoundingClientRect().top <= 600) {
+                    item.classList.add('reveal-item--is-visible');
+                }
+                
+           });
+        
+        }); 
+    }
+    
+}
+
+var revealOnScroll = new RevealOnScroll();
+
+
+/* StickyHeader.js*/
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = document.querySelector('.site-header');
+        this.headerTriggerElement = document.querySelector('.large-hero__title');
+        this.darkenHeader();
+        this.pageSections = document.querySelectorAll('.page-section');
+        this.navSections = document.querySelectorAll('.primary-nav a');
+        this.createPageSectHighlight();
+    }
+    
+    darkenHeader() {
+        document.addEventListener('scroll', () => {
+            if (this.headerTriggerElement.getBoundingClientRect().top <= 0) {
+            this.siteHeader.classList.add('site-header--dark');
+            } else {
+                this.siteHeader.classList.remove('site-header--dark');
+            }
+        });
+        
+    }
+    
+    createPageSectHighlight() {
+        this.pageSections.forEach( (pageSect) => {
+            document.addEventListener('scroll', (e) => {
+                for (let i = 0; i < this.navSections.length; i++) {
+                    if (this.navSections[i].getAttribute("id") === pageSect.getAttribute('data-matching-link').slice(1)) {
+                       if (pageSect.getBoundingClientRect().top <= 5 && pageSect.getBoundingClientRect().top >= pageSect.getBoundingClientRect().height * -1 + 5) {
+                  this.navSections[i].classList.add('is-current-link');
+               } else {
+                   this.navSections[i].classList.remove('is-current-link');
+               } 
+                    }
+                }
+               
+            });
+        });
+    }
+}
+
+
+var stickyHeader = new StickyHeader();
+
+
+
+
+
